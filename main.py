89a442fc7565433f7d7d746340d689f8e39e0a5b
aritraban21 <aritraban10@github.com>
2025-10-07 17:13:52 +0200

    Added basic 2d augmentations

--- main.py
+++ main.py
@@ -114,6 +114,7 @@ def setup(args) -> tuple[nn.Module, Any, Any, Any, DataLoader, DataLoader, int]:
                              root_dir,
                              img_transform=img_transform,
                              gt_transform= partial(gt_transform, K),
+                             augment = args.augment,
                              debug=args.debug)
     train_loader = DataLoader(train_set,
                               batch_size=B,
@@ -124,6 +125,7 @@ def setup(args) -> tuple[nn.Module, Any, Any, Any, DataLoader, DataLoader, int]:
                            root_dir,
                            img_transform=img_transform,
                            gt_transform=partial(gt_transform, K),
+                           augment = False,
                            debug=args.debug)
     val_loader = DataLoader(val_set,
                             batch_size=B,
@@ -277,6 +279,7 @@ def main():
     parser.add_argument('--debug', action='store_true',
                         help="Keep only a fraction (10 samples) of the datasets, "
                              "to test the logics around epochs and logging easily.")
+    parser.add_argument('--augment', action='store_true',help="Enable data augmentation during training.")
 
 
     # arguments related to loss functions/optimizers
