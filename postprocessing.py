61de17ae18fbf80e58af866fe4ae8885a80003d4
Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>
2025-10-08 16:27:12 +0200

    add log lines

--- postprocessing.py
+++ postprocessing.py
@@ -8,20 +8,24 @@ def postprocess(volume, config):
         volume = volume.cpu().numpy()
 
     if config.get('cca', False):
+        print('Performing CCA')
         connected_components = get_connected_components(volume)
         final_volume = np.zeros_like(volume)
 
         for c, labeled in connected_components.items():
             if c in config.get('ignore_classes_cca', []):
+                print(f'Skipping CCA for class {c}')
                 final_volume[volume == c] = c
                 continue
             components_size = np.bincount(labeled.flatten())
             if c in config['cca_threshold']:
                 #for esophagus pick size > 500
+                print(f'Performing CCA for class {c} with threshold {config["cca_threshold"][c]}')
                 filtered_component = np.where(components_size[1:] > config['cca_threshold'][c])[0] + 1
                 mask = np.isin(labeled, filtered_component)
             else:
                 #pick largest component for other organs
+                print(f'Performing CCA for class {c} with largest component')
                 largest_component = components_size[1:].argmax() + 1
                 mask = (labeled == largest_component)
 
@@ -30,11 +34,13 @@ def postprocess(volume, config):
         volume = final_volume
 
     if config.get('fill_holes', False):
+        print(f'Performing filling holes with connectivity {config.get("connectivity", 1)} and radius {config.get("radius", 1)}')
         filled_volume = np.zeros_like(volume)
         structure = generate_binary_structure(len(volume.shape), config.get('connectivity', 1))
         structure = iterate_structure(structure, config.get('radius', 1))
         for cls in range(1, volume.max() + 1):
             if cls in config.get('ignore_classes_fill_holes', []):
+                print(f'Skipping fill_holes for class {cls}')
                 filled_volume[volume == cls] = cls
                 continue
 
