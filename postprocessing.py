7b809664a9a14fe9cf0e1f178b8d3d60d42085db
Leo Horns <leonard.horns@student.uva.nl>
2025-10-11 21:55:07 +0200

    Implement Postprocessing (#2)
    * created postprocessing including morphology and probability smoothing
    
    * add prepare file metrics
    
    * bug fix
    
    * fix relative symbolic link to absolute
    
    * add file to test performance of postprocess
    
    * update postprocessing.py
    
    * bug fix
    
    * add closing morphology to postprocessing
    
    * try fill in holes after cca
    
    * add connectivity to fill holes
    
    * bug fix
    
    * add radius
    
    * add ignore class param
    
    * update for best run
    
    * add extra params
    
    * add log lines
    
    * pass in params
    
    * add log lines
    
    * add check
    
    ---------
    
    Co-authored-by: Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>

---
+++ postprocessing.py
@@ -0,0 +1,113 @@
+import numpy as np
+from scipy.ndimage import label, binary_fill_holes, generate_binary_structure, iterate_structure
+
+
+def postprocess(volume, config):
+
+    if not isinstance(volume, np.ndarray):
+        volume = volume.cpu().numpy()
+
+    if config.get('cca', False):
+        print('Performing CCA')
+        connected_components = get_connected_components(volume)
+        final_volume = np.zeros_like(volume)
+
+        for c, labeled in connected_components.items():
+            if c in config.get('ignore_classes_cca', []):
+                print(f'Skipping CCA for class {c}')
+                final_volume[volume == c] = c
+                continue
+            components_size = np.bincount(labeled.flatten())
+            if c in config['cca_threshold']:
+                #for esophagus pick size > 500
+                print(f'Performing CCA for class {c} with threshold {config["cca_threshold"][c]}')
+                filtered_component = np.where(components_size[1:] > config['cca_threshold'][c])[0] + 1
+                mask = np.isin(labeled, filtered_component)
+            else:
+                #pick largest component for other organs
+                print(f'Performing CCA for class {c} with largest component')
+                largest_component = components_size[1:].argmax() + 1
+                mask = (labeled == largest_component)
+
+            final_volume[mask] = c
+
+        volume = final_volume
+
+    if config.get('fill_holes', False):
+        print(f'Performing filling holes with connectivity {config.get("connectivity", 1)} and radius {config.get("radius", 1)}')
+        filled_volume = np.zeros_like(volume)
+        structure = generate_binary_structure(len(volume.shape), config.get('connectivity', 1))
+        structure = iterate_structure(structure, config.get('radius', 1))
+        for cls in range(1, volume.max() + 1):
+            if cls in config.get('ignore_classes_fill_holes', []):
+                print(f'Skipping fill_holes for class {cls}')
+                filled_volume[volume == cls] = cls
+                continue
+
+            mask = (volume == cls)
+            mask_filled = binary_fill_holes(mask, structure=structure)
+            filled_volume[mask_filled] = cls
+
+        print(f'Check equal {np.array_equal(volume, filled_volume)}')
+        volume = filled_volume
+
+    return volume
+
+def get_connected_components(volume):
+
+    connected_components = {}
+    structure = np.ones((3, 3, 3))
+    for c in range(1, volume.max() + 1):
+        mask = (volume == c)
+        labeled, _ = label(mask, structure)
+
+        connected_components[c] = labeled
+
+    return connected_components
+
+
+def dice_score_3d(pred, target, smooth=1e-6):
+    pred = pred.astype(bool)
+    target = target.astype(bool)
+
+    intersection = np.logical_and(pred, target).sum()
+    dice = (2.0 * intersection + smooth) / (pred.sum() + target.sum() + smooth)
+    return dice
+
+def multiclass_dice(pred, target, num_classes):
+    dice_scores = []
+    for c in range(num_classes):
+        pred_c = (pred == c)
+        target_c = (target == c)
+        dice = dice_score_3d(pred_c, target_c)
+        dice_scores.append(float(dice))
+    return dice_scores
+
+def print_dice_scores(dice_scores):
+    class_names = ["Background", "Esophagus", "Heart", "Trachea", "Aorta"]
+
+    print("🩻 Dice Scores per Class")
+    print("| Class       | Dice Score |")
+    print("|--------------|------------:|")
+
+    for name, score in zip(class_names, dice_scores):
+        print(f"| {name:<12} | {score:.4f} |")
+
+    # Compute mean (excluding background)
+    mean_dice = np.mean(dice_scores[1:])
+    print(f"\nMean (excluding background): {mean_dice:.4f}")
+
+# def apply_morphology(segmentation_mask, operation, radius):
+#
+#     mask = segmentation_mask.cpu().numpy()
+#     processed = np.zeros_like(mask)
+#     selem = morphology.ball(radius)
+#     for c in range(num_classes):
+#         mask = one_hot[c].astype(bool)
+#         if 'opening' in operation:
+#             mask = morphology.binary_opening(mask, selem)
+#         if 'closing' in operation:
+#             mask = morphology.binary_closing(mask, selem)
+#         processed[c] = mask
+#
+#     return torch.tensor(processed).to(segmentation_mask.device)
---
+++ test_postprocess.py
@@ -0,0 +1,76 @@
+import argparse
+import os
+from pathlib import Path
+import nibabel as nib
+import numpy as np
+from postprocessing import postprocess
+
+def main(args):
+
+    src_folder = args.src_folder
+    dest_folder = args.dest_folder
+    if args.threshold is not None:
+        if args.threshold == - 1:
+            threshold = {
+                3: 20,
+                4: 20
+            }
+        else:
+            threshold = {
+                cls: args.threshold
+                for cls in range(1, 5) if args.threshold > 0
+            }
+    else:
+        threshold = None
+
+    os.makedirs(dest_folder, exist_ok=True)
+
+    for patient_file in os.listdir(src_folder):
+        if not patient_file.endswith('.nii.gz'):
+            continue
+        print(f'Processing {patient_file}')
+        pred_path = src_folder / patient_file
+
+        pred_nib = nib.load(pred_path)
+        pred = np.asarray(pred_nib.dataobj)
+
+        config = {
+            'cca': args.cca,
+            'cca_threshold': threshold,
+            'fill_holes': args.fill_holes,
+            'ignore_classes_fill_holes': args.ignore_classes_fill_holes,
+            'ignore_classes_cca': args.ignore_classes_cca,
+            'connectivity': args.connectivity if args.connectivity is not None else 1,
+            'radius': args.radius if args.radius is not None else 1
+        }
+        processed_pred = postprocess(pred, config)
+
+        nib.save(
+            nib.Nifti1Image(processed_pred,
+                            header=pred_nib.header,
+                            affine=pred_nib.affine,
+                            dtype=pred_nib.get_data_dtype()),
+            (dest_folder / patient_file)
+        )
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='File path parameters')
+    parser.add_argument('--src_folder', type=Path, required=True)
+    parser.add_argument('--dest_folder', type=Path, required=True)
+    parser.add_argument('--threshold', type=int, required=False)
+    parser.add_argument('--connectivity', type=int, required=False)
+    parser.add_argument('--radius', type=int, required=False)
+    parser.add_argument('--ignore_classes_fill_holes', type=int, nargs='+', required=False, default=[])
+    parser.add_argument('--ignore_classes_cca', type=int, nargs='+', required=False, default=[])
+    parser.add_argument('--cca', action='store_true', default=False, required=False)
+    parser.add_argument('--fill_holes', action='store_true', default=False, required=False)
+
+    args = parser.parse_args()
+
+    print(args)
+
+    return args
+
+
+if __name__ == "__main__":
+    main(get_args())
